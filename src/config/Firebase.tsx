e045c3b24019c871030599ef815d41094de713a5
Kaneku cat <tamkukumanu@yahoo.com>
2023-07-18 15:05:35 +0700

    bruh

--- src/config/Firebase.tsx
+++ src/config/Firebase.tsx
@@ -5,21 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzrcGd3p1WWQKe0zCcgwRrn4SeeVFNb-s",
-
-  authDomain: "valaint-aa920.firebaseapp.com",
-
-  databaseURL: "https://valaint-aa920-default-rtdb.firebaseio.com",
-
-  projectId: "valaint-aa920",
-
-  storageBucket: "valaint-aa920.appspot.com",
-
-  messagingSenderId: "148390222129",
-
-  appId: "1:148390222129:web:166e43a19e630b8855f810",
-
-  measurementId: "G-Y7FW5BR7TV",
+//add ur stuff here lol
 };
 
 // Initialize Firebase
